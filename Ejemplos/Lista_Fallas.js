c01e09ace5c34f51161fc036c3c561b28660520b
David <berpeda@alumni.uv.es>
2024-02-22 18:44:57 +0100

    Lista de cada falla con nombre, id, section, tipo

--- Ejemplos/Lista_Fallas.js
+++ Ejemplos/Lista_Fallas.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import {FlatList, Text, View} from 'react-native';
+import {StyleSheet, FlatList, Text, View} from 'react-native';
+
 
 const Lista_Fallas = () => {
     const [Fallas, setFallas] = useState([]);
@@ -13,8 +14,15 @@ const Lista_Fallas = () => {
         fetch('https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/falles-fallas/exports/json?lang=es&timezone=Europe%2FBerlin')
             .then((response) => response.json())
             .then((responseJson) => {
-                console.log("mensaje consola"+ responseJson.features);
-                setFallas(responseJson);
+
+                const fallasConTipo = responseJson.map(falla => ({
+                    ...falla,
+                    tipo: "Mayor" 
+                }));
+
+                console.log("Falla Mayor"+ responseJson);
+                
+                setFallas(fallasConTipo);
             }
             )
     }
@@ -24,41 +32,58 @@ const Lista_Fallas = () => {
         fetch(('https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/falles-infantils-fallas-infantiles/exports/json?lang=es&timezone=Europe%2FBerlin'))
             .then((response) => response.json())
             .then((responseJson) => {
+                const fallasConTipo = responseJson.map(falla => ({
+                    ...falla,
+                    tipo: "Infantil" 
+                }));
+
                 console.log("Falla Infantil"+ responseJson);
-                setFallasInfantil(responseJson);
+                setFallasInfantil(fallasConTipo);
             }
             )
     }
     const combinedData = [...Fallas, ...FallasInfantil];
 
-    const FallasItem = ({ item }) => (
-        <View>
-            <Text numberOfLines={1} ellipsizeMode='tail'>
-            {item.nombre} - {item.objectid}
-            
-            </Text>
-            <Text>------------------</Text>
-        </View>
-    );
+    const FallasItem = ({ item, index }) => {
+        const estilo = index % 2 === 0 ? styles.par : styles.impar;
+        return (
+            <View style={estilo}>
+                <Text numberOfLines={4} ellipsizeMode='tail'>
+                    Nombre: {item.nombre}{'\n'}
+                    Id: {item.objectid}{'\n'}
+                    Sección: {item.seccion}{'\n'}  
+                    Tipo: {item.tipo}       
+                </Text>
+            </View>
+        );
+    };
     
 
     return (
-        <View>
-        <FlatList
-            data={combinedData}
-            renderItem={FallasItem}
-            keyExtractor={item =>  item.objectid}
-
-        />
-        
-        
-        
+        <View >
+            <FlatList
+                data={combinedData}
+                renderItem={FallasItem}
+                keyExtractor={item =>  item.objectid}
+            />
         </View>
-
-        
-        
-        
     );
 };
+
+const styles = StyleSheet.create({
+    par:{
+        width: '100%',
+        backgroundColor: '#EFEFEF',
+        padding: 10,
+       
+    },
+    impar:{
+        width: '100%',
+        backgroundColor: 'white',
+        padding: 10,
+    },
+});
+
+
 
 export default Lista_Fallas;
