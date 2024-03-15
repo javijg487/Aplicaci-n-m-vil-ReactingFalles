1771d44e9c794766103d202a4e1f4a5328103b9b
Javier Jiménez <javijg487@gmail.com>
2024-03-15 19:48:16 +0100

    agregado visitado

--- Ejemplos/Datos.js
+++ Ejemplos/Datos.js
@@ -12,11 +12,13 @@ export const DatosProvider = ({ children }) => {
     const [combinedData, setCombinedData] = useState([]);  //Se inicializa con un array vacío
     const [Posicion, setPosicion] = useState(null);
     const [Distancia, setDistancia] = useState(null);
+    const [FallasVisitadas, setFallasVisitadas] = useState([]);
 
     useEffect(() => {
         loadData();
         loadData_Infantiles();
         obtenerPosicion();
+        loadVisitedFallas();
     }, []);
 
     //Si Fallas o FallasInfantil cambian, se actualiza combinedData
@@ -28,10 +30,22 @@ export const DatosProvider = ({ children }) => {
         if (Posicion) {
             calcularDistancia(Posicion); // Si tienes la posición, calcula la distancia
         }
-        requestLocationPermission(); 
+        requestLocationPermission();
     }, [Posicion, combinedData]);
 
-    const obtenerPosicion =  async () => {
+    const saveVisited = async (Falla_Visitada) => {
+        try {
+            await AsyncStorage.setItem(
+                `Falla_Visitada_${Falla_Visitada.objectid}`,
+                JSON.stringify(Falla_Visitada)
+
+            );
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const obtenerPosicion = async () => {
         let location = await Location.getCurrentPositionAsync({});
         setPosicion(location);
     }
@@ -51,7 +65,7 @@ export const DatosProvider = ({ children }) => {
                 const fallasConTipo = responseJson.map(falla => ({
                     ...falla,
                     tipo: "Mayor",
-                    visitado: false,
+                   
                     nombre: falla.nombre || "Nombre no disponible",
                     seccion: falla.seccion || "Sección no disponible",
                     fallera: falla.fallera || "Fallera no disponible",
@@ -75,7 +89,6 @@ export const DatosProvider = ({ children }) => {
                 const fallasConTipo = responseJson.map(falla => ({
                     ...falla,
                     tipo: "Infantil",
-                    visitado: false,
                     nombre: falla.nombre || "Nombre no disponible",
                     seccion: falla.seccion || "Sección no disponible",
                     fallera: falla.fallera || "Fallera no disponible",
@@ -89,19 +102,29 @@ export const DatosProvider = ({ children }) => {
     }
 
     const toggleVisited = (falla) => {
-        setCombinedData(combinedData.map(item => {
-            if (item.objectid == falla.objectid) {
-                falla.visitado ? falla.visitado = false : falla.visitado = true;
+        const index = Distancia.findIndex(item => item.objectid === falla.objectid);
+        if (index !== -1) {
+            const updatedData = [...Distancia];
+            updatedData[index] = {
+                ...updatedData[index],
+                visitado: !updatedData[index].visitado
+            };
+            setCombinedData(updatedData);
+            saveVisited(updatedData[index]);
+            loadVisitedFallas();
+        }
 
-                return { ...item }
-            }
+    };
 
-            return item;
-        }));
-    }
+    const loadVisitedFallas = async () => {
+        const keys = await AsyncStorage.getAllKeys();
+        const visitedFallasKeys = keys.filter(key => key.startsWith('Falla_Visitada_'));
+        const visitedFallas = await AsyncStorage.multiGet(visitedFallasKeys);
+        setFallasVisitadas(visitedFallas.map(([key, value]) => JSON.parse(value)));
+    };  
 
     const FallaVisited = () => {
-        return Distancia.filter(falla => falla.visitado === true);
+        return FallasVisitadas.filter(falla => falla.visitado === true);
     }
 
     const calcularDistancia = (location) => {
@@ -111,14 +134,11 @@ export const DatosProvider = ({ children }) => {
                     { latitude: location.coords.latitude, longitude: location.coords.longitude },
                     { latitude: item.geo_point_2d.lat, longitude: item.geo_point_2d.lon }
                 );
-                return { ...item, distancia: distancia/1000 };
+                return { ...item, distancia: distancia / 1000 };
             });
             setDistancia(nuevasDistancias);
         }
     }
-    
-    
-
 
     return (
         <DatosContext.Provider value={{ combinedData, Fallas, FallasInfantil, toggleVisited, loadData, loadData_Infantiles, setFallas, setFallasInfantil, FallaVisited, Distancia }}>
